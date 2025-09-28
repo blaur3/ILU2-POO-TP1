c20d04806a8dc85650d280863377fda566ec0aa4
blaur3 <145001668+blaur3@users.noreply.github.com>
2025-09-28 11:26:01 +0200

    fix: exception etalOccupe dans methode acheterProduit()

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -1,15 +1,30 @@
 package histoire;
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
 import villagegaulois.Etal;
 
 public class ScenarioCasDegrade {
 	public static void main(String[] args) { 
+		Village village = new Village("le village des irréductibles", 10, 5);
+		Gaulois bonemine = new Gaulois("Bonemine", 7);
+		Chef abraracourcix = new Chef("Abraracourcix", 10, village);
+		
 		Etal etal = new Etal();
 		try {
 		etal.libererEtal(); }
 		catch(NullPointerException e) {
 			e.printStackTrace();
 		}
+		
+		village.ajouterHabitant(bonemine);
+		village.ajouterHabitant(abraracourcix);
+		System.out.println(etal.acheterProduit(10, abraracourcix));
 		System.out.println("Fin du test"); 
-		} 
+		
+		}
+	
 
+	
+	
 }
--- src/villagegaulois/Etal.java
+++ src/villagegaulois/Etal.java
@@ -53,31 +53,36 @@ public class Etal {
 	}
 
 	public String acheterProduit(int quantiteAcheter, Gaulois acheteur) {
-		if (etalOccupe) {
-			StringBuilder chaine = new StringBuilder();
-			chaine.append(acheteur.getNom() + " veut acheter " + quantiteAcheter
-					+ " " + produit + " à " + vendeur.getNom());
-			if (quantite == 0) {
-				chaine.append(", malheureusement il n'y en a plus !");
-				quantiteAcheter = 0;
-			}
-			if (quantiteAcheter > quantite) {
-				chaine.append(", comme il n'y en a plus que " + quantite + ", "
-						+ acheteur.getNom() + " vide l'étal de "
-						+ vendeur.getNom() + ".\n");
-				quantiteAcheter = quantite;
-				quantite = 0;
-			}
-			if (quantite != 0) {
-				quantite -= quantiteAcheter;
-				chaine.append(". " + acheteur.getNom()
-						+ ", est ravi de tout trouver sur l'étal de "
-						+ vendeur.getNom() + "\n");
-			}
-			return chaine.toString();
-		}
-		return null;
+	    StringBuilder chaine = new StringBuilder();
+	    try {
+	        chaine.append(acheteur.getNom() + " veut acheter " + quantiteAcheter
+	                + " " + produit + " à " + vendeur.getNom());
+
+	        if (quantite == 0) {
+	            chaine.append(", malheureusement il n'y en a plus !");
+	            quantiteAcheter = 0;
+	        }
+	        if (quantiteAcheter > quantite) {
+	            chaine.append(", comme il n'y en a plus que " + quantite + ", "
+	                    + acheteur.getNom() + " vide l'étal de "
+	                    + vendeur.getNom() + ".\n");
+	            quantiteAcheter = quantite;
+	            quantite = 0;
+	        }
+	        if (quantite != 0) {
+	            quantite -= quantiteAcheter;
+	            chaine.append(". " + acheteur.getNom()
+	                    + ", est ravi de tout trouver sur l'étal de "
+	                    + vendeur.getNom() + "\n");
+	        }
+	    } catch (NullPointerException e) {
+	        e.printStackTrace();
+	        return "";
+	    }
+
+	    return chaine.toString();
 	}
+
 
 	public boolean contientProduit(String produit) {
 		return produit.equals(this.produit);
